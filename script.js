15640ce5aaa9847c98318317934abe4a7f4dc41e
Diego Go <sajudian85@gmail.com>
2023-05-21 02:01:34 -0500

    Modificacion encriptacion para aplicar restriccion

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //FUNCION IMAGENES
 
-var fondos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+var fondos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
-var fondosError = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+var fondosError = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
 
 var fondo = document.getElementById('imagen');
 var indicFondo = 0;
@@ -17,68 +17,41 @@ function fondoError() {
    fondo.src = `./error/${fondosError[indicFondo]}.png`;
 }
 
+function imprimirError() {
+   document.getElementById('pizarra').innerHTML = "¡Ups, no se aceptan mayúsculas, acentos o caracteres especiales!";
+   fondoError();
+   document.getElementById('botoncopiar').style.display = 'none';
+}
+
 /*La letra "e" es convertida para "enter"
 La letra "i" es convertida para "imes"
 "La letra "o" es convertida para "ober
 La letra "a" es convertida para "ai"
 La letra "u" es convertida para "ufat"*/
 
-function imprimirError() {
-   document.getElementById('pizarra').innerHTML = "¡Ups, no se aceptan acentos o caracteres especiales!";
-   fondoError();
-   document.getElementById('botoncopiar').style.display = 'none';
-}
-
 //FUNCIÓN ENCRIPTAR
 var btnEncriptar = document.getElementById('encriptar');
 
-const condiciones = {
-   minusculas: /[^a-z\s]+/g, // Todo menos Letras minusculas y espacios
-   mayusculas: /[^A-ZÀ-ÿ]+[^.,:;?]+/g, // Todo menos mayusculas
-   sinespacios: /[^a-z]+/g, // Todo menos minisculas
-   acentos: /[^à-ÿÀ-ÿ]+/g, // Acentos
-   prueba2: /[^a-z\s.,:;?]+/gm, // Incluyendo simbolos ortografia
-   prueba: /[^a-z\s?]+/gm, // Todo menos letras minisculas, espacios opcionales
-   letras: /[a-z\s]+/g //Solo de la a - z
-}
+const restriccion = /[^a-z\s?]+/gm;
 
 function encriptacion() {
-   let texto1 = document.getElementById('parrafo').value.toLowerCase();
-   let error = condiciones.prueba2.test(texto1);
-   if (texto1.length == 0) {
+   let texto = document.getElementById('parrafo').value.toLowerCase();
+
+   if (texto.length == 0) {
       document.getElementById('pizarra').innerHTML = "No se encontró ningún texto";
       fondoError();
       document.getElementById('botoncopiar').style.display = 'none';
    }
-   else if (texto1.length != 0) {
-      if (error === false) {
-         if (condiciones.prueba.test(texto1) === false) {
-            let textoEncriptado = texto1.replaceAll('e', 'enter');
-            textoEncriptado = textoEncriptado.replaceAll('i', 'imes');
-            textoEncriptado = textoEncriptado.replaceAll('o', 'ober');
-            textoEncriptado = textoEncriptado.replaceAll('a', 'ai');
-            textoEncriptado = textoEncriptado.replaceAll('u', 'ufat');
-            document.getElementById('pizarra').innerHTML = textoEncriptado;
-            cambiarFondo();
-            document.getElementById('botoncopiar').style.display = 'flex';
-            document.getElementById('parrafo').value = "";
-         }
-         else if (condiciones.prueba2.test(texto1) === false) {
-            let textoEncriptado = texto1.replaceAll('e', 'enter');
-            textoEncriptado = textoEncriptado.replaceAll('i', 'imes');
-            textoEncriptado = textoEncriptado.replaceAll('o', 'ober');
-            textoEncriptado = textoEncriptado.replaceAll('a', 'ai');
-            textoEncriptado = textoEncriptado.replaceAll('u', 'ufat');
-            document.getElementById('pizarra').innerHTML = textoEncriptado;
-            cambiarFondo();
-            document.getElementById('botoncopiar').style.display = 'flex';
-            document.getElementById('parrafo').value = "";
-         }
-         else {
-            imprimirError();
-         }
+   else {
+      let error = texto.match(restriccion);
+      if (error == null) {
+         let textoEncriptado = texto.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('o', 'ober').replaceAll('a', 'ai').replaceAll('u', 'ufat');
+         document.getElementById('pizarra').innerHTML = textoEncriptado;
+         cambiarFondo();
+         document.getElementById('botoncopiar').style.display = 'flex';
+         document.getElementById('parrafo').value = "";
       }
-      else if (error === true) {
+      else {
          imprimirError();
       }
    }
@@ -90,42 +63,23 @@ btnEncriptar.addEventListener('click', encriptacion);
 var btnDesencriptar = document.getElementById('desencriptar');
 
 function desencriptacion() {
-   let texto2 = document.getElementById('parrafo').value.toLowerCase();
-   let error = condiciones.prueba2.test(texto2);
-   if (texto2.length == 0) {
+   let texto = document.getElementById('parrafo').value.toLowerCase();
+
+   if (texto.length == 0) {
       document.getElementById('pizarra').innerHTML = "No se encontró ningún texto";
       fondoError();
       document.getElementById('botoncopiar').style.display = 'none';
    }
-   else if (texto2.length != 0) {
-      if (error === false) {
-         if (condiciones.prueba.test(texto2) === false) {
-            let textoDesencriptado = texto2.replaceAll('enter', 'e');
-            textoDesencriptado = textoDesencriptado.replaceAll('imes', 'i');
-            textoDesencriptado = textoDesencriptado.replaceAll('ober', 'o');
-            textoDesencriptado = textoDesencriptado.replaceAll('ai', 'a');
-            textoDesencriptado = textoDesencriptado.replaceAll('ufat', 'u');
-            document.getElementById('pizarra').innerHTML = textoDesencriptado;
-            cambiarFondo();
-            document.getElementById('botoncopiar').style.display = 'flex';
-            document.getElementById('parrafo').value = "";
-         }
-         else if (condiciones.prueba2.test(texto2) === false) {
-            let textoDesencriptado = texto2.replaceAll('enter', 'e');
-            textoDesencriptado = textoDesencriptado.replaceAll('imes', 'i');
-            textoDesencriptado = textoDesencriptado.replaceAll('ober', 'o');
-            textoDesencriptado = textoDesencriptado.replaceAll('ai', 'a');
-            textoDesencriptado = textoDesencriptado.replaceAll('ufat', 'u');
-            document.getElementById('pizarra').innerHTML = textoDesencriptado;
-            cambiarFondo();
-            document.getElementById('botoncopiar').style.display = 'flex';
-            document.getElementById('parrafo').value = "";
-         }
-         else {
-            imprimirError();
-         }
+   else {
+      let error = texto.match(restriccion);
+      if (error == null) {
+         let textoDesencriptado = texto.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ober', 'o').replaceAll('ai', 'a').replaceAll('ufat', 'u');
+         document.getElementById('pizarra').innerHTML = textoDesencriptado;
+         cambiarFondo();
+         document.getElementById('botoncopiar').style.display = 'flex';
+         document.getElementById('parrafo').value = "";
       }
-      else if (error === true) {
+      else {
          imprimirError();
       }
    }
